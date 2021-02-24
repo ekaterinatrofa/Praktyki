a22d2fc492b170fc3ba98faa434b11ea73b51977
Kateryna Trofymenko <ekaterinatrofa@gmail.com>
2021-02-24 13:10:03 +0200

    Initial commit

---
+++ src/com/company/Zadanie1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Marta");
+        System.out.println("Adam");
+
+    }
+}
