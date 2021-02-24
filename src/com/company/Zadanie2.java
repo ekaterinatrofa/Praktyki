76abdf5005caa08d3f0261734a08d8489ce4ce73
Kateryna Trofymenko <ekaterinatrofa@gmail.com>
2021-02-24 13:13:54 +0200

    commit

---
+++ src/com/company/Zadanie2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        double liczba_1 = 5.5;
+        double liczba_2 = 6.5;
+
+        double wynik = liczba_1 * liczba_2;
+        System.out.println(wynik);
+    }
+
+}
